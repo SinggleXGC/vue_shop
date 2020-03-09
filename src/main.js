2d4dcbbb6cf9434efeb0d013bb0d77f03df255bc
SinggleXGC <1307708161@qq.com>
2020-03-09 20:50:56 +0800

    实现登录表单布局

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from "@/router";
+//导入全局样式
+import './assets/css/global.css'
+//导入element配置文件
+import '@/plugins/element'
+//导入element样式
+import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
---
+++ src/plugins/element.js
@@ -0,0 +1,4 @@
+import Vue from "vue";
+import {Form, FormItem, Input,Button} from "element-ui";
+
+Vue.use(Form).use(FormItem).use(Input).use(Button)
