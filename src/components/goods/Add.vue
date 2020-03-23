b2beb8d25048b08c39b195f7237c0dcc1dcc4963
SinggleXGC <1307708161@qq.com>
2020-03-23 20:21:48 +0800

    完成商品列表

---
+++ src/components/goods/Add.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>123</div>
+</template>
+
+<script>
+    export default {
+        name: "Add",
+        data() {
+            return {
+
+            }
+        },
+        created(){
+
+        },
+        methods: {
+
+        }
+    }
+</script>
+
+<style lang="less" scoped>
+
+</style>
